a43c71f9f0210191a948477a1d6979e0e51e013d
Soljira <soljiragit@gmail.com>
2025-01-11 22:48:24 +0800

    Fix bottom navigation bar but no functionality yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.13.0-alpha09")
 }
