8be0906ddb71c97b005cc8429c4a612a17dd9d74
fpaulpalis <fpaulpalis@gmail.com>
2025-01-11 22:15:15 +0800

    scholarship page 1; :>

---
+++ app/src/main/java/com/example/ite393exam/scholarships/ScholarshipPage1.kt
@@ -0,0 +1,20 @@
+package com.example.ite393exam.scholarships
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ScholarshipPage1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.scholarship_page1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
