e14d66d92562f1c823495c9e01b34fe6ff495d27
Gwen <jilbertcolcoldanao@gmail.com>
2025-01-11 08:34:47 +0800

    profile

--- app/src/main/java/com/example/ite393exam/MainActivity.kt
+++ app/src/main/java/com/example/ite393exam/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.ite393exam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var isEditable = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +21,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        println("Hello")
+        val nameField = findViewById<EditText>(R.id.editTextText)
+        val campusField = findViewById<EditText>(R.id.editTextText2)
+        val courseField = findViewById<EditText>(R.id.editTextText3)
+        val emailField = findViewById<EditText>(R.id.editTextText4)
+
+        val editButton = findViewById<ImageButton>(R.id.imageButton5)
+
+        editButton.setOnClickListener {
+            isEditable = !isEditable
+            nameField.isEnabled = isEditable
+            campusField.isEnabled = isEditable
+            courseField.isEnabled = isEditable
+            emailField.isEnabled = isEditable
+            if (isEditable) {
+                editButton.setImageResource(R.drawable.ch)
+            } else {
+                editButton.setImageResource(R.drawable.edit)
+            }
+        }
     }
 }
