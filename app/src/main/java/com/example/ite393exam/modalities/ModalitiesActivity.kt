523de5326dc11929ff1c42d3f53a4a98e14e9418
Soljira <soljiragit@gmail.com>
2025-01-15 23:14:21 +0800

    Revert "Fix the bug where the components in the Modality tab disappear whenever the back button is pressed after opening one of the buttons"
    This reverts commit 147976d132c97426ec173d20c5b6acc0b4312182.

--- app/src/main/java/com/example/ite393exam/modalities/ModalitiesActivity.kt
+++ app/src/main/java/com/example/ite393exam/modalities/ModalitiesActivity.kt
@@ -7,6 +7,7 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
 import com.example.ite393exam.R
 import com.example.ite393exam.helpers.BottomNavigationHelper
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -44,13 +45,6 @@ class ModalitiesActivity : AppCompatActivity() {
             fragmentBeginTransaction(flexRemoteFragment)
         }
 
-        // Fixes my stupid code below that hides the linear layout when the user opens a new fragment
-        supportFragmentManager.addOnBackStackChangedListener {
-            if (supportFragmentManager.backStackEntryCount == 0) {
-                // Show the linear layout when the back stack is empty
-                showOtherComponents()
-            }
-        }
 
     }
 
@@ -68,9 +62,4 @@ class ModalitiesActivity : AppCompatActivity() {
     fun hideOtherComponents() {
         linearLayout.visibility = View.GONE
     }
-
-    fun showOtherComponents() {
-        linearLayout.visibility = View.VISIBLE
-    }
 }
-
