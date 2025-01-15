61eecaaa1973a6a9b5be46cb2638ae9bfd202585
Cyrus <cyruzjr1205@gmail.com>
2025-01-15 19:22:31 +0800

    added flex remote desc

---
+++ app/src/main/java/com/example/ite393exam/courses/EducAndLibArtsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ite393exam.courses
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ite393exam.R
+
+class EducAndLibArtsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_educ_lib_arts)
+    }
+}
--- app/src/main/java/com/example/ite393exam/modalities/RemoteCoursesActivity.kt
+++ app/src/main/java/com/example/ite393exam/modalities/RemoteCoursesActivity.kt
@@ -1,9 +1,15 @@
 package com.example.ite393exam.modalities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ite393exam.R
+import com.example.ite393exam.courses.CrimJusticeRadActivity
+import com.example.ite393exam.courses.EducAndLibArtsActivity
+import com.example.ite393exam.courses.InfoTechActivity
+import com.example.ite393exam.courses.ManagementAccountancyActivity
 import com.example.ite393exam.helpers.BottomNavigationHelper
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -20,7 +26,29 @@ class RemoteCoursesActivity : AppCompatActivity() {
         val menuItem = bottomNavigationView.menu.findItem(R.id.nav_modalities)
         menuItem.isChecked = true  // ensures that map button thingy stays checked
 
+        val buttonManageAcc: Button = findViewById(R.id.button_management_Acc)
+        val buttonCriminalJustice: Button = findViewById(R.id.button_bs_crim)
+        val buttonInfoTech: Button = findViewById(R.id.button_bs_it)
+        val buttonCela: Button = findViewById(R.id.button_bs_educ_liberal_arts)
 
+        buttonManageAcc.setOnClickListener{
+            navigateToActivity(ManagementAccountancyActivity::class.java)
+        }
 
+        buttonCriminalJustice.setOnClickListener {
+            navigateToActivity(CrimJusticeRadActivity::class.java)
+        }
+
+        buttonInfoTech.setOnClickListener{
+            navigateToActivity(InfoTechActivity::class.java)
+        }
+
+        buttonCela.setOnClickListener{
+            navigateToActivity(EducAndLibArtsActivity::class.java)
+        }
+    }
+    private fun navigateToActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
     }
 }
