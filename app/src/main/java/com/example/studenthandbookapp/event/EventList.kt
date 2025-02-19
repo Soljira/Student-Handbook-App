f3a2f9b7ac70723e5a64ee245e398e88ad022975
Cyrus <cyruzjr1205@gmail.com>
2025-02-20 01:48:59 +0800

    Add Event list layout

--- app/src/main/java/com/example/studenthandbookapp/event/EventList.kt
+++ app/src/main/java/com/example/studenthandbookapp/event/EventList.kt
@@ -1,6 +1,8 @@
 package com.example.studenthandbookapp.event
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studenthandbookapp.R
@@ -9,7 +11,12 @@ class EventList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.)
+        setContentView(R.layout.event_list)
 
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val options = listOf("Show: Upcoming", "Show: Past")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options)
+        spinner.adapter = adapter
+        spinner.setSelection(0)
     }
 }
