7f914c0d7141809222a8d81bba3e1e8115c0eb84
Soljira <soljiragit@gmail.com>
2025-02-19 22:14:21 +0800

    Enable Compose and add necessary Compose dependencies
    - Added the Compose Compiler Gradle plugin in `build.gradle.kts`.
    - Applied the Compose plugin in `app/build.gradle.kts`.
    - Enabled Compose in `app/build.gradle.kts` by setting `compose = true` in `buildFeatures`.
    - Set `kotlinCompilerExtensionVersion` to "1.5.15" in `composeOptions`.
    - Added the compose Bom dependency, along with other compose dependencies (androidx.activity:activity-compose, androidx.compose.ui:ui, androidx.compose.ui:ui-tooling-preview, androidx.compose.material3:material3) and updated the `libs.versions.toml`.
    - Updated the `libs.versions.toml` to have the `compose-compiler` plugin, and `uiTooling` version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.map.secret)
 
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 }
 
 android {
@@ -21,6 +22,11 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -34,6 +40,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -62,4 +73,18 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")  // note: remove this later if hindi na need based sa requirements ni sir
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
+
+    // Jetpack Compose stuff
+    // Specify the Compose BOM with a version definition
+    val composeBom = platform("androidx.compose:compose-bom:2025.01.01")
+    implementation(composeBom)
+    implementation("androidx.activity:activity-compose")
+    //This dependency includes the fundamental UI elements and features provided by Jetpack Compose.
+    implementation("androidx.compose.ui:ui")
+    //This includes tooling and preview functionalities for Compose, assisting with development and debugging UI components.
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    //This dependency includes the Material Design 3 components and styles adapted for Jetpack Compose, allowing the implementation of Material Design principles in your app's UI
+    implementation("androidx.compose.material3:material3")
+    debugImplementation(libs.androidx.ui.tooling)
+//    implementation("androidx.compose.compiler")
 }
