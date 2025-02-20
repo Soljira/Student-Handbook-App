1d3bf04b39b72fd9962ecbe97178b3c70e227eaf
Soljira <soljiragit@gmail.com>
2025-02-20 13:39:23 +0800

    Refactor Event List Layout with Navigation Components
    - Replaced `ConstraintLayout` with `DrawerLayout` for supporting navigation drawer.
    - Added a `MaterialToolbar` as the top app bar and configured it with a menu icon, a title, and a menu.
    - Introduced a `NavigationView` for the navigation drawer, including a header layout and a menu.
    - Implemented a `BottomNavigationView` for bottom navigation.
    - Implemented `TopAppBarHelper`, `BottomNavigationHelper`, and `DrawerNavigationHelper` to set up the top app bar, bottom navigation, and drawer navigation, respectively.
    - Set up the event list layout to include a date display, a "+ New Event" button, a spinner for event filters, and buttons for event display.
    - Updated `EventList` activity to initialize and handle the new navigation components.
    - Added `onResume` to unselect the home icon in the bottom nav when navigating from other drawers.
    - Added `unselectBottomNavIcon()` to make sure no icons are checked on the bottom nav on page load.

--- app/src/main/java/com/example/studenthandbookapp/event/EventList.kt
+++ app/src/main/java/com/example/studenthandbookapp/event/EventList.kt
@@ -5,13 +5,26 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.studenthandbookapp.R
+import com.example.studenthandbookapp.helpers.BottomNavigationHelper
+import com.example.studenthandbookapp.helpers.DrawerNavigationHelper
+import com.example.studenthandbookapp.helpers.TopAppBarHelper
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class EventList : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var topAppBar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.event_list)
+        initializeNavigationStuff()
 
         val spinner: Spinner = findViewById(R.id.spinner)
         val options = listOf("Show: Upcoming", "Show: Past")
@@ -19,4 +32,31 @@ class EventList : AppCompatActivity() {
         spinner.adapter = adapter
         spinner.setSelection(0)
     }
+
+    override fun onResume() {
+        super.onResume()
+        unselectBottomNavIcon()
+
+    }
+
+    fun initializeNavigationStuff() {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navigationView = findViewById(R.id.navigation_view)
+        topAppBar = findViewById(R.id.topAppBar)
+
+        TopAppBarHelper.setupTopAppBar(this, topAppBar, drawerLayout, "Events")
+        BottomNavigationHelper.setupBottomNavigation(this, bottomNavigationView)
+        DrawerNavigationHelper.setupDrawerNavigation(this, drawerLayout, navigationView)
+
+        unselectBottomNavIcon()
+    }
+
+    // ENSURES NA HINDI MACHECEHCK UNG ICONS SA BOTTOM NAVIGATION BAR KAPAG NAGNAVIGATE KA THRU SIDE DRAWER
+    fun unselectBottomNavIcon() {
+        bottomNavigationView.menu.setGroupCheckable(0, true, false)
+        bottomNavigationView.menu.findItem(R.id.nav_home).isChecked = false
+        bottomNavigationView.menu.setGroupCheckable(0, true, true)
+    }
+
 }
